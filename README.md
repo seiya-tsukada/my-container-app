7d66aa2fbfe2797a6768e6b73276fc5d22661f64
Tsukada <seiya.tsukada@accenture.com>
2018-09-22 10:56:05 +0900

    add app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# my-container-app
+# my_container_app
+
+## start command
+
+```
+export FLASK_APP=/var/app/main.py
+flask run
+```
+
+```
+export FLASK_APP=/var/app/main.py
+python -m flask run
+```
---
+++ main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello main application"
+
+@app.route("/sub")
+def hello_world():
+    return "Hello sub application"
