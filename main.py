d56055f25568da71caae4a9bb3e7e14d99a07870
root <root@ip-172-31-48-149.ap-northeast-1.compute.internal>
2018-12-23 10:45:23 +0000

    commit

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #! /usr/bin/env python
 # coding: utf-8
 
-from flask import Flask
+from flask import Flask, request, render_template
+from pprint import pprint
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello main application"
+
+    req_info = dict()
+    req_info["user_agent"] = request.headers.get("User-Agent")
+    req_info["accept"] = request.headers.get("Accept")
+    req_info["accept_language"] = request.headers.get("Accept-Language")
+    req_info["accept_encoding"] = request.headers.get("Accept-Encoding")
+
+    return render_template("index.html", req_info=req_info)
 
 @app.route("/sub")
 def sub():
